026b33fb3ec5d0536f8ee7a2745fe740317e1193
rptetzloff <ray@raytetzloff.com>
2025-09-21 13:58:30 -0500

    Fix countdown to show correct minutes

--- main.js
+++ main.js
@@ -350,7 +350,7 @@ class PackersTracker {
                 
                 let countdownText = '⏰ ';
                 if (days > 0) {
-                    countdownText += `${days}d ${hours}h`;
+                    countdownText += `${days}d ${hours}h ${minutes}m`;
                 } else if (hours > 0) {
                     countdownText += `${hours}h ${minutes}m`;
                 } else {
